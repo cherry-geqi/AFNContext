5954fdc721ccfdd67fb3549f76c445cb558b69fd
xian <xian.tuxoid@qq.com>
2015-08-13 09:33:09 +0800

    response callback

--- AFNContext/AFNContext.h
+++ AFNContext/AFNContext.h
@@ -16,20 +16,35 @@
 - (AFNContext * (^)(NSString *method))method;
 
 - (AFNContext * (^)(NSString *prefix))prefix;
-- (AFNContext * (^)(NSString *path))path;
+- (AFNContext * (^)(NSString *path, ...))path;
 
 - (AFNContext * (^)(NSString *key, id value))addParameter;
 - (AFNContext * (^)(NSDictionary *parameters))addParameters;
 
-- (AFNContext * (^)(NSData *data, NSString *name, NSString *fileName, NSString *mimeType))addMultipartFileData;
-- (AFNContext * (^)(NSURL *fileURL, NSString *name, NSError * __autoreleasing * error))addMultipartFileURL;
-- (AFNContext * (^)(NSURL *fileURL, NSString *name, NSString *fileName, NSString *mimeType, NSError * __autoreleasing * error))addMultipartFileURL2;
-- (AFNContext * (^)(NSInputStream *inputStream, NSString *name, NSString *fileName, int64_t length, NSString *mimeType))addMultipartInputStream;
 - (AFNContext * (^)(void (^)(id<AFMultipartFormData> formData)))addMultipartFormData;
 
 - (AFNContext * (^)(void(^)(CGFloat progress)))uploadProgress;
 - (AFNContext * (^)(void(^)(CGFloat progress)))downloadProgress;
 
-- (void (^)(void (^completionBlock)(id responseObject, NSError *error)))exec;
+- (AFNContext * (^)(void (^responseCallback)(id responseObject, NSError *error, void (^next)(id responseObject, NSError *error))))addResponseCallback;
+
+- (void (^)())done;
+
+@end
+
+@interface AFNContext (AFNContextMultipartExtension)
+
+- (AFNContext * (^)(NSData *data, NSString *name, NSString *fileName, NSString *mimeType))addMultipartFileData;
+- (AFNContext * (^)(NSURL *fileURL, NSString *name, NSError * __autoreleasing * error))addMultipartFileURL;
+- (AFNContext * (^)(NSURL *fileURL, NSString *name, NSString *fileName, NSString *mimeType, NSError * __autoreleasing * error))addMultipartFileURL2;
+- (AFNContext * (^)(NSInputStream *inputStream, NSString *name, NSString *fileName, int64_t length, NSString *mimeType))addMultipartInputStream;
+
+@end
+
+@interface AFNContext (AFNContextResponseExtension)
+
+- (AFNContext * (^)(void(^)(id responseObject)))success;
+- (AFNContext * (^)(void(^)(NSError *error)))failure;
+- (AFNContext * (^)(void(^)(id responseObject, NSError *error)))response;
 
 @end
